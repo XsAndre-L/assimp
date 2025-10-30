a3105d8fbc1fc3ed9fead7e396e9808abe734b5f
xsandre-l <ahloubser12@gmail.com>
2025-10-30 08:50:58 +0200

    .

--- .codeframe/package.ts
+++ .codeframe/package.ts
@@ -5,7 +5,7 @@ import { resolve, join } from "node:path";
 import { argv } from "node:process";
 
 export const build = (cwd: string = process.cwd()): BuildType => {
-  const LINUX = resolve(cwd, "../../toolchains/linux");
+  const TOOLCHAINS = resolve(cwd, "../../toolchains/cmake-tools");
   const toolchain_clang = resolve(cwd, "../../toolchains/dependencies/clang");
   const CLANG = join(toolchain_clang, "bin/clang.exe").replace(/\\/g, "/");
   const CLANGXX = join(toolchain_clang, "bin/clang++.exe").replace(/\\/g, "/");
@@ -60,7 +60,7 @@ export const build = (cwd: string = process.cwd()): BuildType => {
     },
     linux_x86_64: {
       configStep: `cmake -S . -B build/linux/x86_64 -G Ninja \
-      -DCMAKE_TOOLCHAIN_FILE=${LINUX}/linux_x86-64.cmake \
+      -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAINS}/linux_x86-64.cmake \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_COMPILER=${CLANG} \
       -DCMAKE_CXX_COMPILER=${CLANGXX} \
@@ -78,7 +78,7 @@ export const build = (cwd: string = process.cwd()): BuildType => {
     },
     linux_aarch64: {
       configStep: `cmake -S . -B build/linux/aarch64 -G Ninja \
-      -DCMAKE_TOOLCHAIN_FILE=${LINUX}/linux_aarch64.cmake \
+      -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAINS}/linux_aarch64.cmake \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_COMPILER=${CLANG} \
       -DCMAKE_CXX_COMPILER=${CLANGXX} \
